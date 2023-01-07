51d8bfbf956c1cc0600e0c40a72c36c424f34f24
lo-kiss <115636509+lo-kiss@users.noreply.github.com>
2023-01-07 15:38:57 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,113 @@
+"use strict";
+
+let token;
+const serverId = "01F7ZSBSFHQ8TA81725KQCSDDP";
+
+/* Helper functions */
+function qs(e) { return document.querySelector(e); }
+
+window.onload = () => {
+	token = localStorage.getItem("token");
+	if (token) login();
+}
+
+/* Take token and login */
+qs(".token-form").addEventListener("submit", e => {
+	e.preventDefault();
+	token = qs("#token-input").value;
+	login();
+});
+
+/* Delete token */
+qs(".delete-token").addEventListener("click", () => {
+	qs("#token-input").value = "";
+	qs(".saved-token").style.display = "none";
+	token = undefined;
+	localStorage.removeItem("token");
+});
+
+async function getUser() {
+	const res = await fetch("https://api.revolt.chat/users/@me", {
+		headers: { 'x-session-token': token },
+		method: 'GET',
+	});	
+	return res.json();
+}
+
+async function getServer() {
+	const res = await 
+		fetch(`https://api.revolt.chat/servers/${serverId}?`, {
+			headers: { "x-session-token": token, },
+			method: "GET",
+		});
+
+	return res.json();
+}
+
+async function searchChannel(userId, channel) {
+	const res = await
+		fetch(`https://api.revolt.chat/channels/${channel}/search`, {
+			headers: { 'x-session-token': token },
+			method: 'POST',
+			body: JSON.stringify({
+				query: userId,
+				limit: 2,
+				sort: 'Latest',
+				include_users: true,
+			}),
+		});
+
+	return res.json();
+}
+
+async function login() {
+	let user = await getUser();
+	let userId = user._id;
+	renderUser(user);
+
+	localStorage.setItem("token", token);
+	qs(".saved-token").style.display = "block";
+	qs(".mentions").style.display = "block";
+
+	let server = await getServer();
+	// console.log(server);
+	for (let i = 0; i < server.channels.length; i++) {
+		// console.log(server.channels[i]);
+		const data = await searchChannel(userId, server.channels[i]);
+		renderMention(data);
+	}
+}
+
+function renderUser(data) {
+	qs(".username").innerHTML = 
+		`logged in as <b>${data.username}</b> [${data._id}]`;
+}
+
+function renderMention(data) {
+	console.log(data);
+	const mentionContainer = qs(".mentions");
+
+	for (let i = 0; i < data.messages.length; i++) {
+		let mBody = document.createElement('div');
+		let mUsername = document.createElement('p');
+		let mMessage = document.createElement('p');
+		let mLink = document.createElement('a');
+
+		mBody.classList.add("mention");
+		mUsername.classList.add("m-username");
+		mMessage.classList.add("m-message");
+		mLink.classList.add("m-link");
+
+		let urlChan = data.messages[i].channel;
+		let urlMsg = data.messages[i]._id;
+
+		mUsername.innerText = data.messages[i].author;
+		mMessage.innerText = data.messages[i].content;
+		mLink.innerText = "open in revolt";
+		mLink.href = `https://app.revolt.chat/channel/${urlChan}/${urlMsg}`;
+
+		mBody.append(mUsername, mMessage, mLink);
+		mentionContainer.appendChild(mBody);
+	}
+}
+
